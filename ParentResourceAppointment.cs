86059d143f451d713a4e0a24a6039f1426b300fc
Chadrick Mahaffey <chadrickm@gmail.com>
2025-02-23 08:38:52 -0500

    Create ParentResourceAppointment.cs

---
+++ ParentResourceAppointment.cs
@@ -0,0 +1,18 @@
+public class ParentResourceAppointment : Wrapper<Appointment>
+{
+    private ParentResourceAppointment(ResourceAppointment resourceAppt, Guid parentResourceId) : base(resourceAppt.Entity)
+    {
+        if (resourceAppt.SelectedAppointmentResources.All(r => r.Resource.Id != parentResourceId))
+            throw new Exception($"At least one resource must have Id {parentResourceId}");
+    }
+
+    public IReadOnlyCollection<SelectedAppointmentResource> SelectedAppointmentResources => Entity.SelectedAppointmentResources;
+    public DateTime BookedStart => Entity.BookedStart;
+    public DateTime BookedEnd => Entity.BookedEnd;
+
+    public static ParentResourceAppointment From(ResourceAppointment resourceAppt, Guid parentResourceId)
+        => new ParentResourceAppointment(resourceAppt, parentResourceId);
+
+    public ResourceAppointment AsResourceAppointment()
+        => ResourceAppointment.From(Entity);
+}
